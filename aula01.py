d19967d6d1beee4c94dabe95b6c0b7b70032b6ba
Gabriela Ropelatto <gabriela.nunes01097462@sesisenaipr.org.br>
2026-02-19 10:04:33 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 16 # variável inteira 
+peso = 70.1 # variável float 
+nome = "Gabriela Ropelatto Nunes" # variável string 
+sexo_feminino = True # variável bool 
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
