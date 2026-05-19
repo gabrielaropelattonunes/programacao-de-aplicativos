72b821c97dabeb65f57f382f48aa2745567801c9
gabriela <gabriela.nunes01097462@sesisenaipr.org.br>
2026-05-19 11:47:14 -0300

    conteudo atualizado

--- json2.py
+++ json2.py
@@ -1,8 +1,25 @@
-import json
+import json 
 
-with open("notas.json","w"):
-    notas = {"matematica":8.5, "portugues":9.0}
+notas = {
+    "portugues":9.0,
+    "matematica":8.5,
+    "soma":0
+}
 
-with open("notas.json","r") as n:
-    soma = notas["matematica"] + notas["portugues"]
-    print("Soma das notas:", soma)
+matematica = notas["matematica"]
+portugues = notas["portugues"]
+
+matematica = notas ["matematica"]
+portugues = notas ["portugues"]
+
+
+soma_devalor = matematica + portugues
+notas["soma"] = soma_devalor
+
+with open('notas.json','a') as arquivo:
+    json.dump(notas,arquivo,ensure_ascii=False)
+
+print("notas carregadas : ")
+print(f"metmatica: {matematica}")
+print(f"portugues : {portugues}")
+print(f"soma das notas : {soma_devalor}")
