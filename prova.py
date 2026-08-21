9a680bb94f9b1e1086a40fb993cb0e594eaa9d83
gabriela <gabriela.nunes01097462@sesisenaipr.org.br>
2026-08-21 10:09:31 -0300

    correcao com assert

--- prova.py
+++ prova.py
@@ -27,20 +27,18 @@ def cadastrar_tabelas():
         conexao.close()
     
     
-def cadastrar_rede():
+def cadastrar_rede (nome_rede,site):
     try:
         conexao = sqlite3.connect("redes.db")
         cursor = conexao.cursor()
         cursor.execute("PRAGMA foreign_keys = ON;")
-
-        nome_rede = input("digite o nome da rede de academia: ")
-        site = input("digite o nome do site: ")
 
         cursor.execute('''INSERT INTO redes_cinema 
                        (nome_rede,site) VALUES (?,?)''', (nome_rede,site)
                        )
         
         conexao.commit()
+        return "rede cadastrada"
 
     except sqlite3.IntegrityError:
         print("Erro: a rede informada não existe.")
@@ -65,7 +63,8 @@ def listar_redes():
                 print("Nome:", rede[1])
                 print("Site:", rede[2])
             conexao.commit()
-            
+            return "listado com sucesso"
+
     except SyntaxError:
         print("Erro: falha na digitação")
     except NameError:
@@ -74,27 +73,24 @@ def listar_redes():
         conexao.close()
     
     
-def alterar_redes():
+def alterar_redes(ID_atual,nova_rede,novo_site):
     try: 
         conexao = sqlite3.connect("redes.db")
         cursor = conexao.cursor()
         cursor.execute("PRAGMA foreign_keys = ON;")
 
-        ID_atual = int(input("Digite o ID atual: "))
         cursor.execute(f''' SELECT * FROM redes_cinema WHERE ID={ID_atual}''')
         redes = cursor.fetchone()
         if not redes:
             print("nenhuma rede encontrada")     
         else:
-            nova_rede = input("digite o nome da nova rede: ")
-            novo_site = input("digite o nome do novo site: ")
     
             comando = f''' UPDATE redes_cinema SET nome_rede = '{nova_rede}',
                         site = '{novo_site}' WHERE id={ID_atual}'''
 
             cursor.execute(comando)
             conexao.commit()
-            print("novos dados atualizados")
+            return "novos dados atualizados"
     
     except  ValueError:
         print("O ID deve ser um número.")
@@ -106,25 +102,24 @@ def alterar_redes():
         conexao.close()
     
 
-def excluir_redes():
+def excluir_redes(id_redes):
     try:
         conexao = sqlite3.connect("redes.db")
         cursor = conexao.cursor()
         cursor.execute("PRAGMA foreign_keys = ON;")
 
-        id_redes = int(input("Digite o ID da rede: "))
         cursor.execute(f"SELECT * FROM redes_cinema WHERE id = {id_redes}")
 
         redes = cursor.fetchone()
 
         if not redes:
-            print("Nenhuma rede encontrada.")
+            return "Nenhuma rede encontrada."
         else:
             cursor.execute(
                 f"DELETE FROM redes_cinema WHERE id = {id_redes}"
             )
             conexao.commit()
-            print("rede excluida com sucesso! ")
+            return "rede excluida com sucesso! "
         
     except ValueError:
         print("O ID deve ser um número.")
@@ -134,26 +129,24 @@ def excluir_redes():
         conexao.close()   
     
 
-def cadastrar_filial():
+def cadastrar_filial(nome_flial,id_rede):
     try:
         conexao = sqlite3.connect("redes.db")
         cursor = conexao.cursor()
         cursor.execute("PRAGMA foreign_keys = ON;")
 
-        nome_flial = input("digite o nome da filial da academia: ")
-        id_rede = int(input("digite o id da rede de academia: "))
-
         cursor.execute(f"SELECT * FROM redes_cinema WHERE id = {id_rede}")
         rede = cursor.fetchone()
 
         if not rede:
-            print("A rede informada não existe.")
+            return "A rede informada não existe."
         else:
             cursor.execute('''INSERT INTO cinemas_filiais
                         (nome_filial,id_rede) VALUES (?,?)''', (nome_flial,id_rede)
                         )
             conexao.commit()
-            print("filial cadastrada")
+            return "filial cadastrada"
+
     except  sqlite3.Error as erro:
         print("Erro ao criar as tabelas:", erro)
     except sqlite3.IntegrityError:
@@ -171,14 +164,15 @@ def listar_filial():
         filiais = cursor.fetchall()
 
         if len(filiais) == 0:
-            print("Nenhuma filial cadastrada.")
+            return "Nenhuma filial cadastrada."
         else:
             for filial in filiais:
                 print("ID:", filial[0])
                 print("Nome da filial:", filial[1])
                 print("ID da rede:", filial[2])
                 conexao.commit()
-
+            return "listado com sucesso"
+    
     except SyntaxError:
         print("Erro: falha na digitação")
     except NameError:
@@ -187,7 +181,7 @@ def listar_filial():
         conexao.close()
     
 
-def alterar_filial():
+def alterar_filial(ID_filial,nova_filial,novo_id_rede):
     try: 
         conexao = sqlite3.connect("redes.db")
         cursor = conexao.cursor()
@@ -198,7 +192,7 @@ def alterar_filial():
         cursor.execute(f''' SELECT * FROM cinemas_filiais WHERE ID={ID_filial}''')
         filial = cursor.fetchone()
         if not filial:
-            print("nenhuma filial encontrada")     
+            return "nenhuma filial encontrada"    
         else:
             nova_filial = input("digite o nome da nova filial: ")
             novo_id_rede = int(input("digite o nome do novo id da rede: "))
@@ -207,7 +201,7 @@ def alterar_filial():
             rede = cursor.fetchone()
 
         if not rede:
-            print("nenhuma filial encontrada")
+            return "nenhuma filial encontrada"
         else:
             comando = f''' UPDATE cinemas_filiais 
                     SET nome_filial = '{nova_filial}',
@@ -215,7 +209,7 @@ def alterar_filial():
 
             cursor.execute(comando)
             conexao.commit()
-            print("dados da filial alterados")
+            return "dados da filial alterados"
     
     except  ValueError:
         print("O ID deve ser um número.")
@@ -269,25 +263,36 @@ def menu():
             opcao = input("Digite uma opção: ")
 
             if opcao == "1":
-                cadastrar_rede()
+                nome_rede = input("digite o nome da rede de academia: ")
+                site = input("digite o nome do site: ")
+                cadastrar_rede(nome_rede,site)
 
             elif opcao == "2":
                 listar_redes()
 
             elif opcao == "3":
-                alterar_redes()
+                ID_atual = int(input("Digite o ID atual: "))
+                nova_rede = input("digite o nome da nova rede: ")
+                novo_site = input("digite o nome do novo site: ")
+                alterar_redes(ID_atual,nova_rede,novo_site)
 
             elif opcao == "4":
-                excluir_redes()
+                id_redes = int(input("Digite o ID da rede: "))
+                excluir_redes(id_redes)
 
             elif opcao == "5":
-                cadastrar_filial()
+                nome_flial = input("digite o nome da filial da academia: ")
+                id_rede = int(input("digite o id da rede de academia: "))
+                cadastrar_filial(nome_flial,id_rede)
 
             elif opcao == "6":
                 listar_filial()
 
             elif opcao == "7":
-                alterar_filial()
+                ID_filial = int(input("Digite o ID da filial: "))
+                nova_filial = input("digite o nome da nova filial: ")
+                novo_id_rede = int(input("digite o nome do novo id da rede: "))
+                alterar_filial( ID_filial,nova_filial,novo_id_rede)
 
             elif opcao == "8":
                 excluir_filial()
@@ -304,3 +309,13 @@ def menu():
 
 cadastrar_tabelas()
 menu()
+
+assert cadastrar_rede("mg","mg.ww") == "rede cadastrada"
+assert listar_redes() == "listado com sucesso"
+assert alterar_redes(2,"cm","cm.ww") == "novos dados atualizados"
+assert excluir_redes(4) == "rede excluida com sucesso! "
+assert excluir_redes(1) == "Nenhuma rede encontrada."
+assert cadastrar_filial("lm ",2) == "filial cadastrada"
+assert cadastrar_filial("gm",1) == "A rede informada não existe."
+assert listar_filial() == "listado com sucesso"
+assert listar_filial() == "Nenhuma filial cadastrada."
